32d20eff279e775e80e64144eb273c4c8d0e30f7
mutesialine <ktesialine@gmail.com>
2023-08-28 16:20:41 +0200

    feat:send and receive data from server to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,37 @@
-import io from "socket.io-client"; 
-import { useEffect } from "react";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
-const sendMessage = ()=>{
-socket.emit("send_message",{ message:"hello" })
-}
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
 
- export default function App() {
-     useEffect(()=>{
-     socket.on("receive_message",(data)=>{
-     alert(data.message)
-     })
-     },[])
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, []);
 
   return (
-   <div className="max-w-7xl mx-auto p-24 flex justify-between gap-8">
-    <input placeholder="Message..." className="px-4 py-4 w-full outline-none border border-gray-500 rounded-md"/>
-    <button onClick={sendMessage} className="rounded-full p-4 bg-green-600">send the message</button>
-   </div>
-  )
+    <div className="max-w-7xl bg-green-100 mx-auto p-24 space-y-8 w-full relative ">
+       <div className="space-y-6">
+        <h1 className="text-2xl font-bold">Message:</h1>
+        <p>{messageReceived}</p>
+      </div>
+      <div className="flex gap-x-6">
+        <input
+          placeholder="Message..."
+          className="px-4 py-4 w-full outline-none border border-gray-500 rounded-md"
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button onClick={sendMessage} className="rounded-full p-4 bg-green-600">
+          send
+        </button>
+      </div>
+    </div>
+  );
 }
-
-
